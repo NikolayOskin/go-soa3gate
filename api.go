57230bf06a0cb69c079bcc318d5ed6d900e41d95
Oskin <wskeal@gmail.com>
2021-03-28 12:51:27 +0300

    keep 2 methods: GetPenaltyByBill and GetPenaltiesByType

--- api.go
+++ api.go
@@ -1,65 +1,51 @@
 package soa3gate
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-// GetPenaltiesByBill: Получить штраф по УИН
+// GetPenaltyByBill: Получить бюджетное начисление (штраф, налог, ИПФССП и т.п.) по УИН
 func (a3 *A3) GetPenaltyByBill(bill string, extStatus bool) (Penalty, bool, error) {
 	resp, err := a3.getPenalties(penaltyByBillReq(bill, extStatus, a3.config.authKey))
 	if err != nil {
-		return Penalty{}, false, fmt.Errorf("GetPenaltiesByBill error: %v", err)
+		return Penalty{}, false, fmt.Errorf("GetPenaltyByBill error: %v", err)
 	}
 	if !resp.isOk() {
-		return Penalty{}, false, fmt.Errorf(resp.Response.Result.ResMessage)
+		return Penalty{}, false, fmt.Errorf("GetPenaltyByBill error: %v", resp.Response.Result.ResMessage)
 	}
-	if !resp.hasAny() {
+	if !resp.hasResult() {
 		return Penalty{}, false, nil
 	}
 	return resp.Response.Penalties.Penalty[0], true, nil
 }
 
-// FetchBySTS: Получить штрафы по СТС
-func (a3 *A3) FetchBySTS(sts []string, extStatus bool) (*GetPenaltiesResponse, error) {
-	if extStatus && len(sts) > 2 {
-		return nil, fmt.Errorf("FetchBySTS error: max 2 sts allowed with extStatus true")
+/*
+GetPenaltiesByType: Получить бюджетные начисления по документам определенного типа.
+
+Доступный перечень типов: «vu» - номер ВУ; «sts» - номер СТС; «inn» - ИНН физлица; «snils» - СНИЛС;
+«birthCert» - свидетельство о рождении; «pass» - паспорт; «ip» - исполнительное производство ФССП,
+«rawID» - идентефикатор плательщика в формате ГИС.
+*/
+func (a3 *A3) GetPenaltiesByType(docType string, searchedDocs []string, extStatus bool) ([]Penalty, error) {
+	if extStatus && len(searchedDocs) > 2 {
+		// А3 позволяет запрашивать начисления со статусом квитирования не более, чем по двум документам в запросе
+		return nil, errors.New("GetPenaltiesByType error: max 2 documents allowed with extStatus true")
+	}
+	if !isDocTypeValid(docType) {
+		return nil, errors.New("GetPenaltiesByType error: invalid doctype")
 	}
 
-	var stsDocs []Doc
-	for _, s := range sts {
-		stsDocs = append(stsDocs, Doc{DocPayload{"sts", s}})
+	var documents []Doc
+	for _, doc := range searchedDocs {
+		documents = append(documents, Doc{DocPayload{docType, doc}})
 	}
-	resp, err := a3.getPenalties(penaltiesByDocsReq(stsDocs, extStatus, a3.config.authKey))
+	resp, err := a3.getPenalties(penaltiesByDocsReq(documents, extStatus, a3.config.authKey))
 	if err != nil {
-		return nil, fmt.Errorf("FetchBySTS error: %v", err)
+		return nil, fmt.Errorf("GetPenaltiesByType error: %v", err)
 	}
-	return resp, nil
-}
-
-// FetchByDriverLicenses: Получить штрафы по ВУ
-func (a3 *A3) FetchByDriverLicenses(driverLicenses []string, extStatus bool) (*GetPenaltiesResponse, error) {
-	if extStatus && len(driverLicenses) > 2 {
-		return nil, fmt.Errorf("FetchByDriverLicenses error: max 2 driverLicenses allowed with extStatus true")
+	if !resp.isOk() {
+		return nil, fmt.Errorf("GetPenaltiesByType error: %v", resp.Response.Result.ResMessage)
 	}
-
-	var vuDocs []Doc
-	for _, dl := range driverLicenses {
-		vuDocs = append(vuDocs, Doc{DocPayload{"vu", dl}})
-	}
-	resp, err := a3.getPenalties(penaltiesByDocsReq(vuDocs, extStatus, a3.config.authKey))
-	if err != nil {
-		return nil, fmt.Errorf("FetchByDriverLicenses error: %v", err)
-	}
-	return resp, nil
-}
-
-// GetPenaltiesByDocs: Получить штрафы по документам (см. Doc)
-func (a3 *A3) GetPenaltiesByDocs(docs []Doc, extStatus bool) (*GetPenaltiesResponse, error) {
-	if extStatus && len(docs) > 2 {
-		return nil, fmt.Errorf("GetPenaltiesByDocs error: max 2 docs allowed with extStatus true")
-	}
-
-	resp, err := a3.getPenalties(penaltiesByDocsReq(docs, extStatus, a3.config.authKey))
-	if err != nil {
-		return nil, fmt.Errorf("GetPenaltiesByDocs error: %v", err)
-	}
-	return resp, nil
+	return resp.Response.Penalties.Penalty, nil
 }
